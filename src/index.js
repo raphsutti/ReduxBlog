618df08b624445507214dbfccd49b7768f7349a0
raphsutti <neoster9@gmail.com>
2018-06-21 19:59:30 +1000

    Rendering a list of post

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
