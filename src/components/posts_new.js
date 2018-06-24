4627b9ccf5cf741464ac622d63f3f859549918e6
raphsutti <neoster9@gmail.com>
2018-06-25 08:39:26 +1000

    Create post action creator

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   renderField(field) {
@@ -22,7 +24,7 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    console.log(values);
+    this.props.createPost(values);
   }
 
   render() {
@@ -74,4 +76,6 @@ export default reduxForm({
   validate,
   // name of the form
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null,{ createPost })(PostsNew)
+);
